7454b1971bc2f827042821531a5599e04133cb73
powderblue <zxp1937516045@163.com>
2022-07-29 12:37:43 +0800

    1

--- src/views/commodity/parameter/index.vue
+++ src/views/commodity/parameter/index.vue
@@ -8,15 +8,43 @@
         title="注意：只允许为第三级分类设置相关参数！"
         type="warning"
       >
-        <el-form>
-          <el-form-item label="选择商品分类">
-            <el-cascader
-              :options="options"
-              :props="{ expandTrigger: 'hover' }"
-            ></el-cascader>
-          </el-form-item>
-        </el-form>
       </el-alert>
+      <el-form>
+        <el-form-item label="选择商品分类">
+          <el-cascader
+            :options="CommodityClassList"
+            :props="{
+              checkStrictly: true,
+              expandTrigger: 'hover',
+              value: 'cat_id',
+              label: 'cat_name',
+            }"
+          ></el-cascader>
+        </el-form-item>
+      </el-form>
+
+      <el-tabs v-model="activeName" @tab-click="handleClick">
+        <el-tab-pane label="动态参数" name="first">动态参数</el-tab-pane>
+        <el-tab-pane label="静态属性" name="second">静态属性</el-tab-pane>
+      </el-tabs>
+
+      <el-button
+        style="margin: 15px 0"
+        type="primary"
+        disabled
+        size="mini"
+        round
+        >添加参数</el-button
+      >
+
+      <el-table border style="width: 100%">
+        <el-table-column prop="" label="" type="expand" width="180">
+        </el-table-column>
+        <el-table-column prop="" label="#" width="180"> </el-table-column>
+        <el-table-column prop="" label="分类名称" width="180">
+        </el-table-column>
+        <el-table-column prop="" label="操作"> </el-table-column>
+      </el-table>
     </el-card>
   </div>
 </template>
@@ -27,16 +55,25 @@ export default {
   filters: {},
   components: {},
   data () {
-    return {}
+    return {
+      CommodityClassList: [],
+      activeName: 'first'
+    }
   },
   computed: {},
   watch: {},
-  created () { },
+  created () {
+    this.getCommodityClassList()
+  },
   methods: {
     async getCommodityClassList () {
       const res = await getCommodityClass()
       console.log(res)
+      this.CommodityClassList = res.data.data
     }
+  },
+  handleClick (tab, event) {
+    console.log(tab, event)
   }
 }
 </script>
